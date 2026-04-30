b1e030919b9f5d0aac9e10cc4cf798a4194f310a
Delanie <delaniemheck@gmail.com>
2026-04-30 16:32:04 -0400

    added project exercise files

---
+++ 6.3.4/g2.js
@@ -0,0 +1,5 @@
+let partyLocation = "GARDEN";
+let shiftValue = 3;
+let encryptedMessage = "";
+let decryptedMessage = "";
+let isPartySafe = false;
---
+++ 6.3.4/q3.js
@@ -0,0 +1,5 @@
+let partyLocation = "GARDEN";
+const shiftValue = 3;
+let encryptedMessage = "";
+let decryptedMessage = "";
+let isPartySafe = false;
