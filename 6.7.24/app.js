5a0ab32e60a9db8e48f642af06b709490832444a
Delanie <delaniemheck@gmail.com>
2026-06-03 16:31:40 -0400

    initial commit

---
+++ 6.7.24/app.js
@@ -0,0 +1,13 @@
+const friend = "BRUTUS"
+const shiftValue = 3;
+
+// Step 1
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+// Step 2
+const newIndex = index + shiftValue; // i + 3
+const encryptedFirstLetter = alphabet[newIndex];
+const encryptedMessage = "";
+for (let i = 0; i < friend.length; i++) {
+
+}  
