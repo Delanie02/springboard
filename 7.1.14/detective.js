1d87b5c48c63d0cdff1ae5a46cd7f5ead4b79a97
Delanie <delaniemheck@gmail.com>
2026-06-16 12:11:56 -0400

    updated with my solution

--- 7.1.14/detective.js
+++ 7.1.14/detective.js
@@ -1,13 +1,24 @@
-function mysteryOperation ()
-{
-	const outcome = Math.random(); // Generates a random number between 0 and 1.
+let vacationDays = 0;
 
-	if (outcome < 0.5)
-	{
-		console.log("The operation is completed successfully!");
-	}
-	else
-	{
-		throw new Error("The operation is failed mysteriously!");
-	}
+for (let i = 0; i <= 20; i++) {
+  try {
+    mysteryOperation();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    vacationDays += 3;
+    console.log(vacationDays);
+  }
+}
+
+function mysteryOperation() {
+  const outcome = Math.random(); // Generates a random number between 0 and 1.
+
+  if (outcome < 0.5) {
+    console.log("The operation is completed successfully!");
+    vacationDays += 13;
+  } else {
+    vacationDays += 1;
+    throw new Error("The operation is failed mysteriously!");
+  }
 }
