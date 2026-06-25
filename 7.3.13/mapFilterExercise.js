34d206e9ea0f1afe2ddf1e7c1c20b1e0222f55f5
Delanie <delaniemheck@gmail.com>
2026-06-25 16:22:47 -0400

    my updated exercise solution

--- 7.3.13/mapFilterExercise.js
+++ 7.3.13/mapFilterExercise.js
@@ -6,7 +6,11 @@ Examples:
     doubleValuesWithMap([1,-2,-3]) // [2,-4,-6]
 */
 
-function doubleValuesWithMap(arr) {}
+function doubleValuesWithMap(arr) {
+  return arr.map(function (val) {
+    return val * 2;
+  });
+}
 
 /*
 Write a function called valTimesIndex which accepts an array and returns a new array with each value multiplied by the index it is currently at in the array.
@@ -16,24 +20,41 @@ Examples:
     valTimesIndex([1,-2,-3]) // [0,-2,-6]
 */
 
+function valTimesIndex(arr) {
+  return arr.map(function (val, index) {
+    return val * index;
+  });
+}
+
 /*
 Write a function called extractKey which accepts an array of objects and some key and returns a new array with the value of that key in each object.
 
 Examples:
-    extractKey([{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}], 'name') // ['Elie', 'Tim', 'Matt', 'Colt']
+    extractKey([{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}], 'name') 
+    // ['Elie', 'Tim', 'Matt', 'Colt']
 */
 
-function extractKey(arr, key){
-    
+function extractKey(arr, key) {
+  return arr.map(function (val) {
+    return val[key];
+  });
 }
 
 /*
 Write a function called filterByValue which accepts an array of objects and a key and returns a new array with all the objects that contain that key.
 
 Examples:
-    filterByValue([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner') // [{first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Colt', last:"Steele", isCatOwner: true}]
+    filterByValue([
+    {first: 'Elie', last:"Schoppik"}, 
+    {first: 'Tim', last:"Garcia", isCatOwner: true}, 
+    {first: 'Matt', last:"Lane"}, 
+    {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner') 
+    // [{first: 'Tim', last:"Garcia", isCatOwner: true}, 
+    // {first: 'Colt', last:"Steele", isCatOwner: true}]
 */
 
 function filterByValue(arr, key) {
-
+  return arr.filter(function (val) {
+    return key in val;
+  });
 }
