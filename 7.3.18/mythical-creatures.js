e17dae5366f72e0fbe55b19df51048508de9d73f
Delanie <delaniemheck@gmail.com>
2026-06-29 11:05:16 -0400

    my solution to the first task

--- 7.3.18/mythical-creatures.js
+++ 7.3.18/mythical-creatures.js
@@ -1,7 +1,43 @@
 const mythicalCreatures = [
-	{name: "Dragon", type: "Fire", lastSeen: "Volcano Valley"},
-	{name: "Mermaid", type: "Water", lastSeen: "Coral Caves"},
-	{name: "Unicorn", type: "Land", lastSeen: "Enchanted Forest"},
-	{name: "Griffin", type: "Air", lastSeen: "Highwind Mountains"},
-	{name: "Kraken", type: "Water", lastSeen: "Abyssal Depths"}
+  {
+    name: "Dragon",
+    type: "Fire",
+    lastSeen: "Volcano Valley",
+  },
+  {
+    name: "Mermaid",
+    type: "Water",
+    lastSeen: "Coral Caves",
+  },
+  {
+    name: "Unicorn",
+    type: "Land",
+    lastSeen: "Enchanted Forest",
+  },
+  {
+    name: "Griffin",
+    type: "Air",
+    lastSeen: "Highwind Mountains",
+  },
+  {
+    name: "Kraken",
+    type: "Water",
+    lastSeen: "Abyssal Depths",
+  },
 ];
+
+// Use the find method to locate the first creature of the "Water" type and log its name to the console.
+
+function firstWater(arr) {
+  const foundCreature = mythicalCreatures.find(function (value, index, array) {
+    return value.type === "Water";
+  });
+
+  if (foundCreature) {
+    console.log(foundCreature.name);
+  }
+}
+
+// Use the findIndex method to locate the index of the "Griffin" in the mythical creatures array and log it to the console.
+
+// Use the find method to locate the first creature last seen in "Enchanted Forest".
