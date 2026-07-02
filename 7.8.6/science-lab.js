db37802a062d7560cad51d885533211f167601c4
Delanie <delaniemheck@gmail.com>
2026-07-02 11:54:58 -0400

    my solutions

---
+++ 7.8.6/science-lab.js
@@ -0,0 +1,59 @@
+/* Task 1: Compile Participant Details with Shorthand Property Names */
+// TODO: Construct an object named `participant` with properties for `name`, `age`, and `studyField`. Utilize shorthand property names to simplify your code.
+
+const name = "Tim";
+const age = 41;
+const studyField = "History";
+
+const participant = {
+  name,
+  age,
+  studyField,
+};
+
+console.log(`Name: ${name}, Age: ${age}, Study Field: ${studyField}`);
+
+/* Task 2: Implement a Shorthand Function for Participant Info */
+// TODO: Copy the `participant` object by adding a shorthand method named `displayInfo` that prints the participant's details using `this` and a template string.
+
+const participantCopy = {
+  ...participant,
+  displayInfo() {
+    console.log(
+      `Name: ${this.name}, Age: ${this.age}, Study Field: ${this.studyField}`,
+    );
+  },
+};
+
+participantCopy.displayInfo();
+
+/* Task 3: Implement a Same Shorthand Arrow Function for Participant Info */
+// TODO: Echo the above task with an arrow function. Observe the behavior of `this` and explain your findings.
+/*
+ * Observations:
+ * It returns 'undefined' because arrow functions do not inherit 'this' from, in this case, the participant object.
+ */
+
+const participantCopy2 = {
+  ...participant,
+  displayInfo: () => {
+    console.log(
+      `Name: ${this.name}, Age: ${this.age}, Study Field: ${this.studyField}`,
+    );
+  },
+};
+
+participantCopy2.displayInfo();
+
+/* Task 4: Using Computed Property Names */
+// TODO: Implement a function named `updateParticipantInfo` that takes a property name and value as arguments alongside an object and returns a new object with that property dynamically set.
+
+function updateParticipantInfo(property, value, object) {
+  return {
+    ...object,
+    [property]: value,
+  };
+}
+
+const updatedParticipant = updateParticipantInfo("age", 34, participant);
+console.log(updatedParticipant);
