8e19d85c48f48ec4433aa5969339a1792b897b63
Delanie <delaniemheck@gmail.com>
2026-07-27 08:42:37 -0400

    through task 4 solutions

--- 8.3.17/solution.js
+++ 8.3.17/solution.js
@@ -4,3 +4,25 @@ const task1 = document.getElementById("task1");
 
 task1.innerText = "It's changed!";
 
+// Task 2
+
+const task2 = document.getElementById("task2");
+
+task2.innerHTML = "<button>Submit</button>";
+
+// Task 3
+
+const body = document.querySelector("body");
+
+body.style.backgroundColor = "#232323";
+
+// Task 4
+
+const items = document.getElementsByClassName("item");
+
+for (let item of items) {
+  item.style.border = "2px solid #ffffff";
+};
+
+// Task 5
+
