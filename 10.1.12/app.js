d86c7a0e6054b700876674f91be5be79c58e4638
Delanie <delaniemheck@gmail.com>
2026-08-10 16:49:53 -0400

    updated with all four solutions

--- 10.1.12/app.js
+++ 10.1.12/app.js
@@ -19,8 +19,6 @@ class Vehicle {
 }
 
 let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
-myFirstVehicle.honk(); // "Beep."
-myFirstVehicle.toString(); // "The vehicle is a Honda Monster Truck from 1999."
 
 // Part 2. Create a class for a car. The Car class should inherit from Vehicle and each car instance should have a property called numWheels which has a value of 4.
 
@@ -32,7 +30,41 @@ class Car extends Vehicle {
 }
 
 let myFirstCar = new Car("Toyota", "Corolla", 2005);
-myFirstCar.toString(); // "The vehicle is a Toyota Corolla from 2005."
-myFirstCar.honk(); // "Beep."
-myFirstCar.numWheels; // 4
 
+// Part 3. Create a class for a Motorcycle. This class should inherit from Vehicle and each motorcycle instance should have a property called numWheels which has a value of 2. It should also have a revEngine method which returns “VROOM!!!”
+
+class Motorcycle extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 2;
+  }
+  revEngine() {
+    return "VROOM!!!";
+  }
+}
+
+let myFirstMotorcycle = new Motorcycle("Honda", "Nighthawk", 2000);
+
+// Part 4. Create a class for a Garage. It should have a property called vehicles which will store an array of vehicles, and a property called capacity which is a number indicating how many vehicles will fit in the garage. When you create a garage, vehicles will always be empty; you only need to provide the capacity.
+
+class Garage {
+  constructor(capacity) {
+    this.vehicles = [];
+    this.capacity = capacity;
+  }
+
+  // A garage should also have an add method, which attempts to add a vehicle to the array of vehicles. However, if you try to add something which is not a vehicle, the garage should return the message “Only vehicles are allowed in here!”. Also, if the garage is at capacity, it should say “Sorry, we’re full.”
+
+  add(vehicle) {
+    if (!(vehicle instanceof Vehicle)) {
+      return "Only vehicles are allowed in here!";
+    } else if (this.vehicles.length >= this.capacity) {
+      return "Sorry, we’re full.";
+    } else {
+      this.vehicles.push(vehicle);
+      return "Vehicle added!";
+    }
+  }
+}
+
+let garage = new Garage(2);
